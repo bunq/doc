ca93663f3c6d3babe5a00d7a8e10974cb563c6d8
Nicola Parrello <nparrello@bunq.com>
2019-03-14 14:34:32 +0100

    bunq/doc#93 - Added .env and GTM snippet

---
+++ src/helpers/tracking.js
@@ -0,0 +1,65 @@
+/**
+ */
+export default function initializeCookieConsentBar() {
+    window.addEventListener("load", () => {
+        var shouldAddMotionListeners = true;
+
+        window.cookieconsent.initialise({
+            position: "bottom",
+            content: {
+                message: "We use cookies when you continue to use our website.",
+                dismiss: "got it!",
+                link: "Learn more",
+                href: "https://www.bunq.com/privacy"
+            },
+            container: document.querySelector(".react-root"),
+            onInitialise: function () {
+                if (this.hasConsented()) {
+                    initializeAllTracking();
+
+                    shouldAddMotionListeners = false;
+                }
+            }
+        });
+
+        if (window.cookieconsent.hasInitialised && shouldAddMotionListeners) {
+            window.addEventListener("scroll", initializeAllTracking);
+            window.addEventListener("keydown", initializeAllTracking);
+            window.addEventListener("mousedown", initializeAllTracking);
+            window.addEventListener("touchstart", initializeAllTracking);
+        }
+    });
+}
+
+/**
+ */
+function initializeGoogleTagManagerIfNeeded() {
+    const googleTagManagerContainerId = process.env.REACT_APP_GOOGLE_TAG_MANAGER_CONTAINER_ID;
+
+    if (googleTagManagerContainerId) {
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                "gtm.start": new Date().getTime(),
+                event: "gtm.js"
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l !== "dataLayer" ? "&l=" + l : "";
+            j.async = true;
+            j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+            f.parentNode.insertBefore(j, f);
+        }) (window, document, "script", "dataLayer", googleTagManagerContainerId);
+    }
+}
+
+/**
+ */
+function initializeAllTracking() {
+    initializeGoogleTagManagerIfNeeded();
+
+    window.removeEventListener("scroll", initializeAllTracking);
+    window.removeEventListener("keydown", initializeAllTracking);
+    window.removeEventListener("mousedown", initializeAllTracking);
+    window.removeEventListener("touchstart", initializeAllTracking);
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 // @flow
 import SwaggerUIStandalonePreset from "swagger-ui";
+import "../node_modules/cookieconsent/build/cookieconsent.min.js";
+import dotenv from "dotenv";
+
+import initializeCookieConsentBar from "./helpers/tracking";
+import registerServiceWorker from "./helpers/registerServiceWorker";
+
 import "swagger-ui/dist/swagger-ui.css";
 
-import registerServiceWorker from "./helpers/registerServiceWorker";
 import {
     BunqLayoutPlugin,
     DisableTryItOutPlugin
 } from "./plugins";
 import "./scss/index.css";
+
+dotenv.config();
 
 SwaggerUIStandalonePreset({
     url: "https://raw.githubusercontent.com/bunq/doc/master/swagger.json",
@@ -20,4 +27,6 @@ SwaggerUIStandalonePreset({
     ]
 });
 
+initializeCookieConsentBar();
 registerServiceWorker();
+
